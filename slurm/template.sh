51e3f29cbbacf0235160711e5d8a933b537b2edd
fortuno <fortuno@users.noreply.github.com>
2017-02-09 18:30:23 +0000

    feat(cwl): get input locations instead signpost

--- slurm/template.sh
+++ slurm/template.sh
@@ -6,8 +6,8 @@
 
 # java_heap="XX_JAVAHEAP_XX"
 
-normal_id="XX_NORMAL_ID_XX"
-tumor_id="XX_TUMOR_ID_XX"
+normal_s3="XX_NORMAL_S3_PATH_XX"
+tumor_s3="XX_TUMOR_S3_PATH_XX"
 ref_fa_id="XX_REF_FA_ID_XX"
 ref_fai_id="XX_REF_FAI_ID_XX"
 ref_dict_id="XX_REF_DICT_ID_XX"
@@ -16,6 +16,8 @@ popfile_tbi_json="XX_POPFILE_TBI_ID_XX"
 case_id="XX_CASE_ID_XX"
 s3dir="XX_S3DIR_XX"
 base_url="XX_BASE_URL_XX"
+input_s3_section="XX_INPUT_S3_SECT_XX"
+output_s3_section="XX_OUTPUT_S3_SECT_XX"
 
 aws_config=${HOME}/.aws/config
 aws_creds=${HOME}/.aws/credentials
@@ -38,8 +40,8 @@ sudo chown ubuntu:ubuntu -R contest_cwl
 trap cleanup EXIT
 
 /home/ubuntu/.virtualenvs/p2/bin/python $wkdir/contest_cwl/slurm/run_cwl.py \
---normal_signpost_id $normal_id \
---tumor_signpost_id $tumor_id \
+--normal_s3_path $normal_s3 \
+--tumor_s3_path $tumor_s3 \
 --ref_dict_signpost_id $ref_dict_id \
 --ref_fa_signpost_id $ref_fa_id \
 --ref_fai_signpost_id $ref_fai_id \
@@ -52,4 +54,6 @@ trap cleanup EXIT
 --basedir $wkdir \
 --signpost_base_url $base_url \
 --s3dir $s3dir \
+--input_s3section $input_s3_section \
+--output_s3section $output_s3_section \
 --cwl $wkdir/contest_cwl/workflow/contest-tool-workflow.cwl.yaml
